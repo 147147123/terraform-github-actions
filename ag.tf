b7c39432379802a1ee5117ce3d497597542c4714
KirylY <Kiryl.Yaroshkin@ahold.com>
2023-09-19 20:01:09 +0300

    fix

--- ag.tf
+++ ag.tf
@@ -1,17 +1,16 @@
 resource "azurerm_monitor_action_group" "ag_error" {
   name                = "NameOfTheActionGroup"
   resource_group_name = azurerm_resource_group.rg-aks.name
-  location            = var.location
+  //location            = var.location
   short_name          = "ag"
   enabled             = true //Check
 
   dynamic "email_receiver" {
     for_each = var.email_receiver_settings
     content {
-      name                    = email_receiver_settings.value["name"]
-      email_address           = email_receiver_settings.value["email_address"]
+      name                    = email_receiver.value["name"]
+      email_address           = email_receiver.value["email_address"]
       use_common_alert_schema = true
     }
   }
-
 }
